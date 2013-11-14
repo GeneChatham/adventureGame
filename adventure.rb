eeeac782c216a8621b0666a819b84a3e4589b969
GeneChatham <genechatham@gmail.com>
2013-11-13 18:32:53 -0600

    finished, playable game

--- adventure.rb
+++ adventure.rb
@@ -33,7 +33,8 @@ class Adventure
 
 	def trial
 		if @destination == "WOODS"
-			puts "You come across a whomping willow that tries to attack you.  What do you do?  RUN, FIGHT, or TALK?"
+			puts "You come across a whomping willow that tries to attack you."
+			self.options
 			action = self.actions
 			if action == "TALK"
 				puts "You calm down the whomping willow with your soothing voice.  It gives you a vast fortune in return for becoming its friend.  YOU WIN!"
@@ -46,7 +47,37 @@ class Adventure
 			end
 		end
 
-		# if @destination == "CASTLE"
+		if @destination == "CASTLE"
+			puts "You come across a ghost that tries to steal your body."
+			self.options
+			action = self.actions
+			if action == "TALK"
+				puts "The ghost is uninterested in talking.  He quickly possesses you."
+				self.dead
+			elsif action == "FIGHT"
+				puts "You have no magical powers.  How could you possibly fight a ghost?"
+				self.dead
+			else
+				puts "Yep, that's your best option.  On your way out of the castle you steal some jewels and gold.  YOU WIN!"
+			end
+		end
+		
+		if @destination == "VILLAGE"
+			puts "As soon as you enter the village, you begin to be mobbed by zombies.  Fast zombies.  Good thing you brought your baseball bat with you today."
+			self.options
+			action = self.actions
+			if action == "TALK"
+				puts "The zombies quickly tear your flesh from your still talking body."
+				self.dead
+			elsif action == "FIGHT"
+				puts "Good for you!  After ten minutes of bashing zombie skulls, you have eradicated the village of the undead.  YOU WIN!"
+			else
+				puts "These are fast zombies, remember?  They quickly cut off your escape and devour your brains."
+				self.dead
+			end
+		end
+
+
 	end
 
 	def dead
@@ -56,6 +87,11 @@ class Adventure
 	def actions
 		action = gets.chomp.to_s.upcase
 	end
+
+	def options
+		puts "What do you do?  RUN, FIGHT, or TALK?"
+	end
+
 end
 
 
